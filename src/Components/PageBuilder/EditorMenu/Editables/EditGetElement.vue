b83abec0eee8975b72f42282ef74118ff04423ad
Qais Wardag <qais.wardag@outlook.com>
2025-09-03 08:39:04 +0200

    Fix: adjust width class for responsive design in EditGetElement component

--- src/Components/PageBuilder/EditorMenu/Editables/EditGetElement.vue
+++ src/Components/PageBuilder/EditorMenu/Editables/EditGetElement.vue
@@ -365,7 +365,7 @@ const openOptionsMoreOpen = ref(false)
     <transition name="popup-fade">
       <div
         v-if="openOptionsMoreOpen"
-        class="pbx-fixed pbx-z-40 pbx-top-10 pbx-right-0 lg:pbx-w-72 md:pbx-w-64 pbx-w-full pbx-select-none"
+        class="pbx-fixed pbx-z-40 pbx-top-10 pbx-right-0 lg:pbx-w-72 md:pbx-w-72 pbx-w-full pbx-select-none"
       >
         <div
           class="pbx-rounded-3xl pbx-border pbx-border-gray-100 pbx-bg-white pbx-shadow-lg pbx-pt-4 pbx-pb-4 pbx-flex pbx-flex-col pbx-overflow-y-auto pbx-max-h-[80vh] pbx-mx-4 pbx-px-2"
