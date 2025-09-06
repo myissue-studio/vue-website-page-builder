73aff2412744f225ba7d2f5d4f7db6896feaa987
Qais Wardag <qais.wardag@outlook.com>
2025-09-06 10:57:20 +0200

    Fix: update positioning of options menu and edit toolbar for better alignment

--- src/Components/PageBuilder/EditorMenu/Editables/EditGetElement.vue
+++ src/Components/PageBuilder/EditorMenu/Editables/EditGetElement.vue
@@ -439,7 +439,7 @@ const handleDelete = function () {
     <transition name="popup-fade">
       <div
         v-if="openOptionsMoreOpen"
-        class="pbx-fixed pbx-z-40 pbx-top-10 pbx-right-[-4rem] lg:pbx-w-72 pbx-w-72 pbx-select-none"
+        class="pbx-top-10 pbx-absolute pbx-z-40 pbx-left-1/2 pbx-transform pbx--translate-x-1/2 pbx-w-72 pbx-select-none"
       >
         <div
           class="pbx-rounded-3xl pbx-border pbx-border-gray-100 pbx-bg-white pbx-shadow-lg pbx-pt-4 pbx-pb-4 pbx-flex pbx-flex-col pbx-overflow-y-auto pbx-max-h-[80vh] pbx-mx-4 pbx-px-2"
