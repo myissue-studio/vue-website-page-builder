81b233988334161e4e2599e649ecaf9422632b34
Qais Wardag <qais.wardag@outlook.com>
2025-09-15 15:26:23 +0200

    Fix: update contact form link in footer

--- src/Components/Homepage/Footer.vue
+++ src/Components/Homepage/Footer.vue
@@ -34,7 +34,7 @@ const version = __APP_VERSION__
       class="pbx-myPrimaryParagraph lg:pbx-text-base pbx-text-xs lg:pbx-font-semibold pbx-font-medium pbx-border-b pbx-border-gray-400"
     >
       <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSd-0aBo4kNjTi9_7HFrMHmMJV93lrrnl6fc67AT7cFWcqZLOQ/viewform"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSeGgtmskw4T7o2Q50C6UCh3EidVbYDhrTXNUMsd6EykpzGTOQ/viewform"
         target="_blank"
         class="pbx-myPrimaryLink pbx-text-myPrimaryDarkGrayColor"
       >
