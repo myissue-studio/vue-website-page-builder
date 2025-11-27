79cebefa40d7567c5b4994354a3a02f29f45d7d0
Qais Wardag <qais.wardag@outlook.com>
2025-11-27 17:02:08 +0100

    fix: update navigation link from 'Examples' to 'Demo' in VitePress config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
+      { text: 'Demo', link: 'https://mybuilder.dev' },
     ],
 
     sidebar: [
