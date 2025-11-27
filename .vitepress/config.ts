945bd2cbbc6a292b63bfe91697befe3525373c3e
Qais Wardag <qais.wardag@outlook.com>
2025-11-27 16:17:59 +0100

    fix: update base path for deployment in VitePress config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
   title: 'Vue 3 Page Builder',
   description:
     'Vue 3 Drag & Drop Page Builder. Power your vision and build impressive, modern pages. A web builder designed for stunning results. Enable users to design and publish responsive pages—such as listings, job posts, or blogs—at any scale. Easily manage and update content with flexibility.',
+  base: '/vue-website-page-builder/',
   themeConfig: {
-    themeConfig: {
-      search: {
-        provider: 'local',
-      },
+    search: {
+      provider: 'local',
     },
+
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
