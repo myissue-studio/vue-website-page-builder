0d4f9cad1e695130aae14e89ec0dd1a1ea610cdd
Qais Wardag <qais.wardag@outlook.com>
2026-06-24 20:39:29 +0200

    Update HTML templates for Blog Post, Stats Simple, and Job Post components to enhance layout and styling

--- src/Components/Loaders/ElementLoader.vue
+++ src/Components/Loaders/ElementLoader.vue
@@ -2,11 +2,6 @@
   <div class="pbx-flex pbx-items-center pbx-justify-center pbx-mt-4">
     <div
       class="pbx-inline-block pbx-h-8 pbx-w-8 pbx-animate-spin pbx-rounded-full pbx-border-4 pbx-border-solid pbx-border-current pbx-border-r-transparent pbx-align-[-0.125em] motion-reduce:pbx-animate-[spin_1.5s_linear_infinite] pbx-text-black dark:pbx-text-white"
-    >
-      <span
-        class="pbx-absolute! pbx-m-px! pbx-h-px! pbx-w-px! pbx-overflow-hidden! pbx-whitespace-nowrap! pbx-border-0! pbx-p-0! [clip:rect(0,0,0,0)]!"
-        >Loading...</span
-      >
-    </div>
+    ></div>
   </div>
 </template>
